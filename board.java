1e7e5482bfdf30c2dfcc16d4c611a21647546953
Josh Owoeye <111530957+troisangle@users.noreply.github.com>
2023-04-21 04:02:33 +0000

    final

--- board.java
+++ board.java
@@ -1,2 +1,16 @@
 public class board{
+    piece[][] b;
+    public board(){
+     b = new piece[8][8];
+    }
+    public boolean isOpen(int x,int y){
+        if(b[x][y]==null){
+            return true;
+        }
+        return false;
+    }
+    public void move(int ox,int oy,int nx,int ny){
+        b[nx][ny]= b[ox][oy];
+        b[ox][oy] = null;
+    }
 }
--- piece.java
+++ piece.java
@@ -5,4 +5,31 @@ public class piece {
     this.x=x;
     this.y=y;
     }
+    public String yCord(int x){
+        if(x==1){
+            return "a";
+        }
+        if(x==2){
+            return "b";
+        }
+        if(x==3){
+            return "c";
+        }
+        if(x==4){
+            return "d";
+        }
+        if(x==5){
+            return "e";
+        }
+        if(x==6){
+            return "f";
+        }
+        if(x==7){
+            return "g";
+        }
+        if(x==8){
+            return "h";
+        }
+      return "error" ;
+    }
 }
